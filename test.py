efcdbe11d6732284f8ee6dcb92a63f0004e109fc
Your Name <your_email@example.com>
2024-11-22 01:22:30 +0600

    first commit

---
+++ test.py
@@ -0,0 +1,196 @@
+######## TASK 60    
+# arr = [10, 20, 30]
+# tar = 5
+# for i in range (len(arr)):
+#         if arr[i] // tar :
+#             print ("Yes")
+#         else :
+#             print ("No")
+
+
+
+######## TASK 59
+# nums = [3, 1, 4, 1, 5]
+# m = min(nums)
+# nums.pop(m)
+# print (nums)
+
+
+
+
+######## TASK 58
+# num1=[]
+# nums = [1, 2, 2, 3, 3, 3]
+# for i in range(len(nums)):
+#     if i % 3 == 0:
+#         num1.append(nums[i])
+# print(set(num1))
+
+
+
+######## TASK 57
+# numbers = [121, 343, 123]
+# output = []
+
+# for num in numbers:
+#     if str(num) == str(num)[::-1]:
+#         output.append(True)
+#     else:
+#         output.append(False)
+
+# print(output)  # [True, True, False]
+
+
+
+######## TASK 56
+# number = 1234
+# digits = []
+
+# for digit in str(number):
+#     digits.append(int(digit))
+
+# print(digits)  
+
+
+
+######## TASK 55
+# digits = [1, 2, 3, 4]
+# number = int(''.join(map(str, digits)))
+# print(number)
+
+
+
+######## TASK 54
+# n1 = [1, 2, 3]
+# n2 = [2, 3, 4]
+# n3 = n1 + n2
+# print (set(n3))
+
+
+
+######## TASK 53
+# arr1 = [1, 2, 3]
+# arr2 = [2, 3, 4]
+
+# i = list(set(arr1) & set(arr2))
+
+# print(i)  
+
+
+
+######## TASK 52
+# n1 = [1,2,3]
+# n2 = [1,2,3]
+
+# if n1 == n2:
+#     print ("Yes")
+# else:
+#     print ("No")
+
+
+
+######## TASK 51
+# arr = [4, 2, 7, 1]
+# a1 = list(set(arr))
+# a1.sort()
+# min2 = a1[1]
+# print (min2)
+
+
+
+######## TASK 50
+# arr = [1, 2, 3, 4]
+# larg = list(set(arr))
+# larg.sort()
+# lar2 = larg[-2]
+# print (lar2)
+
+
+
+######## TASK 49
+# nums = [1, 2, 3]
+# answer = []
+# for i in range(len(nums)):
+#     num = nums[i] * 3
+#     answer.append(num)
+# print(answer)
+
+
+
+######## TASK 48
+# nums = [1, 2, 3, 4]
+# answer = 0
+# nums_len = len(nums)
+# for i in range (len(nums)):
+#     answer += nums[i]
+# print (answer / nums_len)
+
+
+
+######## TASK 47
+# arr = [1, 2, 3, 4]
+
+# if len(arr) == len(set(arr)):
+#     print("Yes")
+# else:
+#     print("No")
+
+
+
+######## TASK 46
+arr = [1, 2, 3, 4, 5, 6]
+size = 2
+
+output = []
+for i in range(0, len(arr), size):
+    output.append(arr[i:i + size])
+
+print(output)  
+
+######## TASK 45
+######## TASK 44
+######## TASK 43
+######## TASK 42
+######## TASK 41
+######## TASK 40
+######## TASK 39
+######## TASK 38
+######## TASK 37
+######## TASK 36
+######## TASK 35
+######## TASK 34
+######## TASK 33
+######## TASK 32
+######## TASK 31
+######## TASK 30
+######## TASK 29
+######## TASK 28
+######## TASK 27
+######## TASK 26
+######## TASK 25
+######## TASK 24
+######## TASK 23
+######## TASK 22
+######## TASK 21
+######## TASK 20
+######## TASK 19
+######## TASK 18
+######## TASK 17
+######## TASK 16
+######## TASK 15
+######## TASK 14
+######## TASK 13
+######## TASK 12
+######## TASK 11
+######## TASK 10
+######## TASK 9
+######## TASK 8
+######## TASK 7
+######## TASK 6
+######## TASK 5
+######## TASK 4
+######## TASK 3
+######## TASK 2
+######## TASK 1
+
+
